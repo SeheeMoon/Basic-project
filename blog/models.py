c972a81ddd1341f4de3e82277cf5920009c60790
SeheeMoon <anstpgml5035@ajou.ac.kr>
2022-08-02 01:11:20 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ class Post(models.Model): # models 모듈의 Model 클래스를 확장해서 만
     title = models.CharField(max_length=30) # CharField 클래스는 문자(char)를 담는 필드
     content = models.TextField() # TextField는 문자열의 길이 제한이 없음
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #이미지를 지정할 폴더의 경로 규칙 지정, 뒤 옵션은 해당 필드는 필수 항목은 아니라는 뜻
     created_at = models.DateTimeField(auto_now_add=True) #DateTimeField는 월, 일, 시, 분, 초까지 기록할 수 있게 해주는 필드, auto_now_add=True는 처음 레코드가 생성될 때 현재시각이 자동으로 저장되게 함
     updated_at = models.DateTimeField(auto_now=True) #auto_now=True는 다시 저장할 때 마다 그 시각이 저장되도록 함
     # author: 추후 작성 예정
