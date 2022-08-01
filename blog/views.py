267e2c18931b1ce06f605c670b41aac234aef7a7
SeheeMoon <anstpgml5035@ajou.ac.kr>
2022-08-01 21:14:15 +0900

    CBV로 블로그 포스트 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
-from .models import Post
+#from django.shortcuts import render FBV코드를 모두 주석처리하였음.
+#from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#def index(request):
+    #posts = Post.objects.all().order_by('-pk')
+    #return render(
+        #request,
+        #'blog/index.html',
+        #{
+            #'posts': posts,
+        #}
+    #)
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
@@ -21,3 +21,12 @@ def single_post_page(request, pk):
             'post': post,
         }
     )
+
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
