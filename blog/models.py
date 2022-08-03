95e05543a91d8e8e1601db1890be09c8115938df
SeheeMoon <anstpgml5035@ajou.ac.kr>
2022-08-03 16:59:18 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 class Post(models.Model): # models 모듈의 Model 클래스를 확장해서 만듦
     title = models.CharField(max_length=30) # CharField 클래스는 문자(char)를 담는 필드
@@ -9,10 +10,10 @@ class Post(models.Model): # models 모듈의 Model 클래스를 확장해서 만
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True) #DateTimeField는 월, 일, 시, 분, 초까지 기록할 수 있게 해주는 필드, auto_now_add=True는 처음 레코드가 생성될 때 현재시각이 자동으로 저장되게 함
     updated_at = models.DateTimeField(auto_now=True) #auto_now=True는 다시 저장할 때 마다 그 시각이 저장되도록 함
-    # author: 추후 작성 예정
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}' #pk는 각 레코드에 대한 고유값 ex)처음 포스트는 자동으로 pk값 1 부여, 두번째 포스트는 2
+        return f'[{self.pk}]{self.title} :: {self.author}' #pk는 각 레코드에 대한 고유값 ex)처음 포스트는 자동으로 pk값 1 부여, 두번째 포스트는 2
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
